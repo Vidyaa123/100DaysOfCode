ec93c4d05f8e8e9cb7a53cbf6098136d202e6c7d
Vidyaa <mailvidyaa@gmail.com>
2021-12-28 14:26:48 +0000

    Updated day-5 code

--- Day-5/hangman.py
+++ Day-5/hangman.py
@@ -9,16 +9,19 @@ words = ['alphabet','behaviour','calling', 'dinosaur','exciting']
 guessed_word = []
 #choose a random word from the list
 word_to_guess = random.choice(words)
+print(word_to_guess)
 # replace letters with blanks
 word_to_guess_len = len(word_to_guess)-1
 for i in range(word_to_guess_len):
     guessed_word.append('_')
-    
 
-letter = str(input("Guess a letter:- ").lower())
-for i in range(word_to_guess_len):
-    if word_to_guess[i] == letter:
-        guessed_word[i] = letter
-    
-print_filled_blank(guessed_word)
+print(guessed_word)   
+while guessed_word == "_":
+    guess = str(input("Guess a letter:- ").lower())
+    for position in range(word_to_guess_len):
+        letter = word_to_guess[position]
+        if letter == guess:
+            guessed_word[position] = letter
 
+    print(guessed_word)
+        
